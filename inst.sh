4faf129793a27b580240a38266b23cdcdb4bbfef
qwestqwert <82978206+qwestqwert@users.noreply.github.com>
2021-04-23 00:54:27 +0300

    Add files via upload

---
+++ inst.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/igroman787/mytonctrl/master/scripts/install.sh
+ls -lh
+sudo bash install.sh -m lite
+# автоустановка
+rm ton-lite-client-test1.config.json
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/ton-lite-client-test1.config.json
+rm mytoncore.db
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.db
+rm /usr/bin/ton/lite-client/ton-lite-client-test1.config.json
+cp ton-lite-client-test1.config.json /usr/bin/ton/lite-client/ton-lite-client-test1.config.json
+rm /usr/local/bin/mytoncore/mytoncore.db
+cp mytoncore.db /usr/local/bin/mytoncore/mytoncore.db
+#
