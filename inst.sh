2a924331e88d9b725d6d04a59135ed07da63f9ce
qwestqwert <82978206+qwestqwert@users.noreply.github.com>
2021-04-27 11:38:23 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -4,7 +4,7 @@ ls -lh
 sudo bash install.sh -m lite
 # автоустановка
 rm ton-lite-client-test1.config.json
-wget https://raw.githubusercontent.com/qwestqwert/unih/main/ton-lite-client-test1.config.json
+wget https://raw.githubusercontent.com/qwestqwert/velia/main/ton-lite-client-test1.config.json
 rm mytoncore.db
 wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.db
 rm /usr/bin/ton/lite-client/ton-lite-client-test1.config.json
