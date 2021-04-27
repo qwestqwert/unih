77a1269789d4131a61bd538937b53eb85baccac8
qwestqwert <82978206+qwestqwert@users.noreply.github.com>
2021-04-28 00:58:08 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/igroman787/mytonctrl/master/scripts/install.sh
-ls -lh
-sudo bash install.sh -m lite
+#wget https://raw.githubusercontent.com/igroman787/mytonctrl/master/scripts/install.sh
+#ls -lh
+#sudo bash install.sh -m lite
 # автоустановка
 rm ton-lite-client-test1.config.json
-wget https://raw.githubusercontent.com/qwestqwert/velia/main/ton-lite-client-test1.config.json
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/ton-lite-client-test1.config.json
 rm mytoncore.db
 wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.db
 rm /usr/bin/ton/lite-client/ton-lite-client-test1.config.json
