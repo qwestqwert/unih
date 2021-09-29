c2f85497ac844bb0b5aaa5ee0b9c213b7ae06808
qwestqwert <82978206+qwestqwert@users.noreply.github.com>
2021-09-29 20:16:10 +0300

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-COLOR='\033[92m'
-ENDC='\033[0m'
-# Копируем файлы
-echo -e "${COLOR}[1/4]${ENDC} Копируем db и py"
-mkdir /usr/src/ton_backup
-cp /usr/local/bin/mytoncore/mytoncore.db /usr/src/ton_backup/mytoncore.db
-cp /usr/src/mytonctrl/mytoncore.py /usr/src/ton_backup/mytoncore.py
-# Удаление
-echo -e "${COLOR}[2/4]${ENDC} Удаляем всё"
-rm -rf /usr/src/pow-miner-gpu
-rm -rf /usr/src/mytonctrl
-rm -rf /usr/bin/ton
-# Ставим GPU
-echo -e "${COLOR}[3/4]${ENDC} Ставим gpu-miner"
+sudo apt -y install software-properties-common
+sudo apt install -y build-essential git make cmake lang libgflags-dev zlib1g-dev libssl-dev libreadline-dev libmicrohttpd-dev pkg-config libgsl-dev python3 python3-dev python3-pip
+sudo pip3 install psutil crc16 requests
+cd /
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
 cd /usr/src
 git clone --recursive https://github.com/tontechio/pow-miner-gpu.git
 mkdir /usr/bin/ton
@@ -27,9 +23,15 @@ wget https://newton-blockchain.github.io/global.config.json
 cd /
 wget https://raw.githubusercontent.com/qwestqwert/unih/main/inst.sh
 sudo bash inst.sh -m lite
-# Копируем файлы
-echo -e "${COLOR}[4/4]${ENDC} Копируем db и py назад"
+COLOR='\033[92m'
+ENDC='\033[0m'
+mkdir /usr/src/ton_backup
+cd /usr/src/ton_backup
+echo -e "${COLOR}[1/2]${ENDC} Качаем модифицированный mytoncore.db"
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.db
 cp /usr/src/ton_backup/mytoncore.db /usr/local/bin/mytoncore/mytoncore.db
+echo -e "${COLOR}[2/2]${ENDC} Качаем модифицированный mytoncore.py"
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.py
 cp /usr/src/ton_backup/mytoncore.py /usr/src/mytonctrl/mytoncore.py
 systemctl restart mytoncore
 # End
