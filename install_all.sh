a0350a709fb9837c3220687e19146149715d9572
qwestqwert <82978206+qwestqwert@users.noreply.github.com>
2021-10-08 17:07:35 +0300

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -9,32 +9,20 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
-cd /usr/src
-git clone --recursive https://github.com/tontechio/pow-miner-gpu.git
-mkdir /usr/bin/ton
-cd /usr/bin/ton
-export CCACHE_DISABLE=1
-export CUDA_HOME=/usr/local/cuda
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64
-export PATH=$PATH:$CUDA_HOME/bin
-cmake -DCMAKE_BUILD_TYPE=Release -DMINERCUDA=true /usr/src/pow-miner-gpu
-make -j 8 pow-miner pow-miner-cuda lite-client
-wget https://newton-blockchain.github.io/global.config.json
-cd /
 wget https://raw.githubusercontent.com/qwestqwert/unih/main/inst.sh
 sudo bash inst.sh -m lite
 apt install -y mc
-#COLOR='\033[92m'
-#ENDC='\033[0m'
-#mkdir /usr/src/ton_backup
-#cd /usr/src/ton_backup
-#echo -e "${COLOR}[1/2]${ENDC} Качаем модифицированный mytoncore.db"
-#wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.db
-#cp /usr/src/ton_backup/mytoncore.db /usr/local/bin/mytoncore/mytoncore.db
-#echo -e "${COLOR}[2/2]${ENDC} Качаем модифицированный mytoncore.py"
-#wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.py
-#cp /usr/src/ton_backup/mytoncore.py /usr/src/mytonctrl/mytoncore.py
-#systemctl restart mytoncore
+COLOR='\033[92m'
+ENDC='\033[0m'
+mkdir /usr/src/ton_backup
+cd /usr/src/ton_backup
+echo -e "${COLOR}[1/2]${ENDC} Качаем модифицированный mytoncore.db"
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.db
+cp /usr/src/ton_backup/mytoncore.db /usr/local/bin/mytoncore/mytoncore.db
+echo -e "${COLOR}[2/2]${ENDC} Качаем модифицированный mytoncore.py"
+wget https://raw.githubusercontent.com/qwestqwert/unih/main/mytoncore.py
+cp /usr/src/ton_backup/mytoncore.py /usr/src/mytonctrl/mytoncore.py
+systemctl restart mytoncore
+echo -e "${COLOR}[0]${ENDC} Готово!"
 # End
-#echo -e "${COLOR}[0]${ENDC} Готово!"
 #
